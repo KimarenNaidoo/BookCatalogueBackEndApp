5ca015425ab9ad2d0a1c58e2c0636aa5c4bbdcde
KimarenNaidoo <kimarennaidoo@gmail.com>
2025-04-04 12:29:34 +0200

    feat: Update API response to use a DTO.

--- src/main/java/com/example/BookCatalogueSpringBootWebApp/controller/BookController.java
+++ src/main/java/com/example/BookCatalogueSpringBootWebApp/controller/BookController.java
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.BookCatalogueSpringBootWebApp.service.BookService;
+import com.example.BookCatalogueSpringBootWebApp.dto.BookDTO;
 import com.example.BookCatalogueSpringBootWebApp.model.Book;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RestController
@@ -17,24 +19,24 @@ public class BookController {
 	public BookService bookService = new BookService();
 
 	@GetMapping("/")
-    public List<Book> getAllBooks() {
-        List<Book> books = bookService.getAllBooks();
-
-		return books;
+    public List<BookDTO> getAllBooks() {
+		return bookService.getAllBooks().stream().map(BookDTO::fromEntity).collect(Collectors.toList());
     }
 
 	@GetMapping("/{id}")
-	public Book getBookById(@PathVariable("id") Long id) {
+	public BookDTO getBookById(@PathVariable("id") Long id) {
 		Book book = null;
+		BookDTO bookDTO = null;
 
 		try {
 			book = bookService.getBookById(id).get();
+			bookDTO = BookDTO.fromEntity(book);
 		} catch(Exception e) {
 			System.err.println("Error: Unable to resolve Book entry with Id: " + id);
 	
 		}
 		
-		return book;
+		return bookDTO;
 	}
 
 }
---
+++ src/main/java/com/example/BookCatalogueSpringBootWebApp/dto/BookDTO.java
@@ -0,0 +1,77 @@
+package com.example.BookCatalogueSpringBootWebApp.dto;
+
+import com.example.BookCatalogueSpringBootWebApp.model.Book;
+import com.example.BookCatalogueSpringBootWebApp.util.BookUtil;
+
+public class BookDTO {
+    
+    private String title;
+    private String status;
+    private String category;
+    private String author;
+    private String ownership;
+    private String isbn;  
+
+    public BookDTO(){}
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getOwnership() {
+        return ownership;
+    }
+
+    public void setOwnership(String ownership) {
+        this.ownership = ownership;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public static BookDTO fromEntity(Book book) {
+        BookDTO dto = new BookDTO();
+        BookUtil bookUtil = new BookUtil(book);
+        dto.setTitle(book.getTitle());
+        dto.setStatus(bookUtil.getStatus());
+        dto.setAuthor(book.getAuthor());
+        dto.setIsbn(book.getIsbn());
+        dto.setOwnership(bookUtil.getOwnership());
+        dto.setCategory(bookUtil.getCategory());
+
+        return dto;
+    }
+}
