9240ca0510409cee2fb8c8a202ec4799167268ea
KimarenNaidoo <kimarennaidoo@gmail.com>
2025-05-06 20:32:07 +0200

    feat(api): Introduce api versioning and logging.

--- src/main/java/com/example/BookCatalogueSpringBootWebApp/controller/BookController.java
+++ src/main/java/com/example/BookCatalogueSpringBootWebApp/controller/BookController.java
@@ -6,25 +6,40 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.BookCatalogueSpringBootWebApp.service.BookService;
+import com.example.BookCatalogueSpringBootWebApp.util.APIUtil;
 import com.example.BookCatalogueSpringBootWebApp.dto.BookDTO;
 import com.example.BookCatalogueSpringBootWebApp.model.Book;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 
 @RestController
-@RequestMapping("/api/books")
+@RequestMapping("/api/v1/books")
 public class BookController {
 
 	private final BookService bookService;
+	private final APIUtil apiComponent;
 
 	public BookController(BookService bookService) {
 		this.bookService = bookService;
+		this.apiComponent = new APIUtil("1");
 	}
 
 	@GetMapping("/")
     public List<BookDTO> getAllBooks() {
-		return bookService.getAllBooks().stream().map(BookDTO::fromEntity).collect(Collectors.toList());
+		List<BookDTO> bookDTOs = new ArrayList();
+
+		try {
+			apiComponent.logApiCall("getAllBooks");
+			bookDTOs = bookService.getAllBooks().stream().map(BookDTO::fromEntity).collect(Collectors.toList());
+		}
+		catch (Exception e) {
+			System.err.println("Error: Unable to retrieve book entries");
+		}
+
+		return bookDTOs;
     }
 
 	@GetMapping("/{id}")
@@ -33,6 +48,7 @@ public class BookController {
 		BookDTO bookDTO = null;
 
 		try {
+			apiComponent.logApiCall("getBookById");
 			book = bookService.getBookById(id).get();
 			bookDTO = BookDTO.fromEntity(book);
 		} catch(Exception e) {
